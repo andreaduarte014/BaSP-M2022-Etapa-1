4bfedb08a5dc0ac34606167b84dd6b5d50d137da
andreaduarte014 <andreaduarte014@gmail.com>
2022-04-16 20:01:08 -0300

    BaSP-2022-W5: ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,12 @@
+/* 1a- create two numeric variables and save the sum of both of them in another one */
+var num1=4;
+var num2=40;
+var sum=num1+num2;
+/* 1b- create two strings variables and put them together in a nuew one */
+var name='Andrea';
+var surname='Duarte';
+var fullName=(name+' '+surname);
+/*1c- crate two strings variables and sum all the characters from both. Save the result in a third variable */
+var word1='software';
+var word2='programer';
+var totalCharacters=(word1.length+word2.length);
